65bd2eb9b8596ed47e601ce9ac619f0783f68be0
dongjunnn <207185421+dongjunnn@users.noreply.github.com>
2025-08-19 19:54:45 +0800

    Level-0

---
+++ src/main/java/Coffee.java
@@ -0,0 +1,14 @@
+public class Coffee {
+    public static void main(String[] args) {
+        String coffee =
+                "  ____       __  __           \n" +
+                        " / ___|___  / _|/ _| ___  ___ \n" +
+                        "| |   / _ \\| |_| |_ / _ \\/ _ \\\n" +
+                        "| |__| (_) |  _|  _|  __/  __/\n" +
+                        " \\____\\___/|_| |_|  \\___|\\___|\n";
+
+        System.out.println(coffee);
+        System.out.println("Hello! I'm Coffee.\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
