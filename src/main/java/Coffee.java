372f3925f7f226be47c659b4d6b8a21fb1a1aa77
dongjunnn <207185421+dongjunnn@users.noreply.github.com>
2025-08-19 21:04:53 +0800

    Level-3

--- src/main/java/Coffee.java
+++ src/main/java/Coffee.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 
 public class Coffee {
     public static void main(String[] args) {
+        String horizontalRule =
+                "---------------------------------------------";
         String coffee =
                 "  ____       __  __           \n" +
                         " / ___|___  / _|/ _| ___  ___ \n" +
@@ -17,23 +19,29 @@ public class Coffee {
         String user_input = "";
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (true) {
             user_input = sc.nextLine();
+            String[] parts = user_input.split("\\s+", 2);
 
-            if (user_input.equals("bye")) {
+
+            if (parts[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
-            } else if (user_input.equals("list")) {
-                for (int i=0; i< taskList.size(); i++) {
-                    System.out.println(i+1 + ". " + taskList.get(i));
+            } else if (parts[0].equals("list")) {
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(i + 1 + ". " + taskList.get(i));
                 }
+            } else if (parts[0].equals("mark")) {
+                taskList.get(Integer.parseInt(parts[1])-1).markAsDone();
+            } else if (parts[0].equals("unmark")) {
+                taskList.get(Integer.parseInt(parts[1])-1).markAsNotDone();
             } else {
                 System.out.println("added: " + user_input);
-                taskList.add(user_input);
+                taskList.add(new Task(user_input));
             }
-            System.out.println();
+            System.out.println(horizontalRule);
         }
     }
 }
