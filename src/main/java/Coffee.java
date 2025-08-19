d733e64d8e94a3b02d78f7b3baac569da05fbf19
dongjunnn <207185421+dongjunnn@users.noreply.github.com>
2025-08-19 20:11:22 +0800

    Level-1

--- src/main/java/Coffee.java
+++ src/main/java/Coffee.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Coffee {
     public static void main(String[] args) {
         String coffee =
@@ -9,6 +11,21 @@ public class Coffee {
 
         System.out.println(coffee);
         System.out.println("Hello! I'm Coffee.\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("What can I do for you?\n");
+
+        String user_input = "";
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            user_input = sc.nextLine();
+
+            if (user_input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            } else {
+                System.out.println(user_input + "\n");
+            }
+
+        }
     }
 }
