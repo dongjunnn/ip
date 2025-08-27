a05d104d43dc9b8e28c4c20f9737e90391ad995b
dongjunnn <207185421+dongjunnn@users.noreply.github.com>
2025-08-27 21:21:57 +0800

    Add datetime functionality

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        this.from = LocalDateTime.parse(from, formatter);
+        this.to   = LocalDateTime.parse(to, formatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        return "[E]" + super.toString() + " (from: " + from.format(fmt)
+                + " to: " + to.format(fmt) + ")";
     }
 
     public String toFileString() {
